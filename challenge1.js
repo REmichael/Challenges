ad9920b00ee179a8772cce4162e6a6f6349858ba
Michael Reardon <michael.hdi@gmail.com>
2019-01-09 16:56:19 -0500

    Challenges

---
+++ challenge1.js
@@ -0,0 +1,64 @@
+// //Challenge Assessment 1
+// ***********************
+
+// What is a palindrome?  A palindrome is a word that is spelled the same way, both forwards and backwards (i.e. bob, level, tacocat, racecar, etc.).  Notice that each of these words have the same spelling if you reversed the word.
+// Using only JavaScript, create a prompt telling the user, "Enter a word to see if it is a palindrome", then create a way to check if the word is in fact a palindrome.  If it is, have JavaScript console.log that their word is a palindrome; if it is not, have JavaScript console.log the opposite.
+
+
+let word1 = "racecar";
+
+let word2 = word1.split("");
+
+let word3 = word2.reverse();
+
+// console.log(word3);
+
+let word4 = word3.join("");
+
+console.log(word4);
+
+if (word1 == word4) {
+    console.log('that word is a palindrome')
+} else {
+    console.log('not a palindrome')
+}
+
+
+
+
+function check_Palindrome(str_entry){
+    // Change the string into lower case and remove  all non-alphanumeric characters
+       var cstr = str_entry.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+        var ccount = 0;
+    // Check whether the string is empty or not
+        if(cstr==="") {
+            console.log("Nothing found!");
+            return false;
+        }
+    // Check if the length of the string is even or odd 
+        if ((cstr.length) % 2 === 0) {
+            ccount = (cstr.length) / 2;
+        } else {
+    // If the length of the string is 1 then it becomes a palindrome
+            if (cstr.length === 1) {
+                console.log("Entry is a palindrome.");
+                return true;
+            } else {
+    // If the length of the string is odd ignore middle character
+                ccount = (cstr.length - 1) / 2;
+            }
+        }
+    // Loop through to check the first character to the last character and then move next
+        for (var x = 0; x < ccount; x++) {
+    // Compare characters and drop them if they do not match 
+            if (cstr[x] != cstr.slice(-1-x)[0]) {
+                console.log("Entry is not a palindrome.");
+                return false;
+            }
+        }
+        console.log("The entry is a palindrome.");
+        return true;
+    }
+    check_Palindrome('madam');
+    check_Palindrome('nurses run');
+    check_Palindrome('fox');
